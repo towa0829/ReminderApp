a74a3708ed6902497e17152fe3a00033d4531223
towa <towa0000829@icloud.com>
2026-05-27 22:20:28 +0900

    Initial Commit

---
+++ Reminder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Reminder
+//
+//  Created by Takashima Towa on 2026/05/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Reminder/ReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderApp.swift
+//  Reminder
+//
+//  Created by Takashima Towa on 2026/05/27.
+//
+
+import SwiftUI
+
+@main
+struct ReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
